cfb5acd6d8d06682a92857abdf6a0684f38f7024
unknown <282854419@qq.com>
2018-04-05 22:07:03 +0800

    GlobalExceptionHandler

---
+++ Test/src/main/java/com/hufan/controller/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.hufan.controller;
+
+import com.hufan.com.hufan.exception.BusinessException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Hufan on 2018/4/5.
+ */
+
+/**
+ * @ControllerAdvice 处理controller中所有没有被try catch包裹的注解
+ * @ResponseBody 返回json格式
+ * @ExceptionHandler 针对哪种类型的异常
+ */
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Map<String, Object> handlerException(){
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("code", 500);
+        map.put("msg", "系统繁忙请稍后再试！");
+        return map;
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    @ResponseBody
+    public Map<String, Object> handlerBusinessException(BusinessException businessException){
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("code", businessException.getCode());
+        map.put("msg", businessException.getMsg());
+        map.put("desc", businessException.getDesc());
+        return map;
+    }
+
+}
--- Test/src/main/java/com/hufan/controller/SampleController.java
+++ Test/src/main/java/com/hufan/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.hufan.controller;
 
+import com.hufan.com.hufan.exception.BusinessException;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
@@ -20,6 +21,27 @@ public class SampleController {
     //@ResponseBody
     String home() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/home1")
+    public String home1() {
+        int i = 2/0;
+        return "home1：全局异常处理";
+    }
+
+    @RequestMapping("/home2")
+    public  String home2() {
+        try{
+            int i = 2/0;
+        }catch (Exception e) {
+
+        }
+        return "home2 i = 2/1，try catch异常捕获";
+    }
+
+    @RequestMapping("/home3")
+    public  String home3() throws BusinessException {
+        throw new BusinessException(404,"地址不存在，请重新检查输入地址！","事物异常处理");
     }
 
     /*public static void main(String[] args) throws Exception {
